166dcff205f2f31cc3879b035a997227da0f4c29
Dan Munckton <dan@cultivatehq.com>
2015-05-06 14:58:56 +0100

    Add distortion with no UI

--- app.js
+++ app.js
@@ -85,6 +85,26 @@
   }
 
   //
+  // Distortion
+  //
+
+  function makeDistortionCurve(amount) {
+    var k = typeof amount === 'number' ? amount : 50,
+        n_samples = 44100,
+        curve = new Float32Array(n_samples),
+        deg = Math.PI / 180,
+        i = 0,
+        x;
+
+    for ( ; i < n_samples; ++i ) {
+      x = i * 2 / n_samples - 1;
+      curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
+    }
+
+    return curve;
+  };
+
+  //
   // MAIN ---------------------------------------------------------------------
   //
 
@@ -97,10 +117,15 @@
       function(stream) {
         console.log('stream.');
         var source = window.__source = audioCtx.createMediaStreamSource(stream);
+        var distortion = audioCtx.createWaveShaper();
         var echo = new SlapBackEcho(audioCtx, document.querySelector("[data-module='echo']"));
         var mixer = new Mixer(audioCtx, document.querySelector("[data-module='mixer']"));
 
-        source.connect(echo.input());
+        distortion.curve = makeDistortionCurve(100);
+        distortion.oversample = '4x';
+
+        source.connect(distortion);
+        distortion.connect(echo.input());
         echo.connect(mixer.input());
         mixer.connect(audioCtx.destination);
       },
