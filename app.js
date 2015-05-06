84ca19273d3804413e8aa0b92df91e93a6d0f783
Dan Munckton <dan@cultivatehq.com>
2015-05-06 20:59:56 +0100

    Create a UI for the panner / LFO

--- app.js
+++ app.js
@@ -109,16 +109,28 @@
   // LFO
   //
 
-  function LFO(audioCtx, paramToModulate) {
+  function LFO(audioCtx, paramToModulate, uiElement) {
+    var self = this;
+
+    this.speedCtrl = uiElement.querySelector('.js-lfo-speed');
+    this.widthCtrl = uiElement.querySelector('.js-lfo-gain');
+
     this.lfo = audioCtx.createOscillator();
     this.lfoGain = audioCtx.createGain();
 
-    this.lfo.frequency.value = 5.0;
-    this.lfoGain.gain.value = 0.0;
+    this.lfo.frequency.value = this.speedCtrl.value;
+    this.lfoGain.gain.value = this.widthCtrl.value;
 
     this.lfo.connect(this.lfoGain);
     this.lfoGain.connect(paramToModulate);
     this.lfo.connect(paramToModulate);
+
+    this.speedCtrl.addEventListener('input', function() {
+      self.lfo.frequency.value = self.speedCtrl.value;
+    });
+    this.widthCtrl.addEventListener('input', function() {
+      self.lfoGain.gain.value = self.widthCtrl.value;
+    });
 
     this.lfo.start();
   }
@@ -158,7 +170,7 @@
         var echo = new SlapBackEcho(audioCtx, document.querySelector("[data-module='echo']"));
         var mixer = new Amplifer(audioCtx, document.querySelector("[data-module='mixer']"));
         var panner = audioCtx.createStereoPanner();
-        var lfo = new LFO(audioCtx, panner.pan);
+        var lfo = new LFO(audioCtx, panner.pan, document.querySelector("[data-module='panner']"));
 
         distortion.curve = makeDistortionCurve(18);
         distortion.oversample = '4x';
