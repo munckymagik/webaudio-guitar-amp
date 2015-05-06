4bd04994b9f109a9a3a882b805c114249634ee43
Dan Munckton <dan@cultivatehq.com>
2015-05-06 13:57:19 +0100

    Support basic input/output of a signal from a guitar
    Note without disabling echoCancellation the signal has what sounds like
    volume modulation on it. On speaking to Paul Adenot this is because the
    default is set up for WebRTC like applications where noise cancellation
    and feedback loop protection are needed.

--- app.js
+++ app.js
@@ -2,12 +2,38 @@
   "use strict";
 
   //
-  // MAIN ----------------------------------------------------------------------
+  // Shims --------------------------------------------------------------------
+  //
+
+  navigator.getUserMedia = (navigator.getUserMedia ||
+                            navigator.webkitGetUserMedia ||
+                            navigator.mozGetUserMedia ||
+                            navigator.msGetUserMedia);
+
+  //
+  // MAIN ---------------------------------------------------------------------
   //
 
   window.addEventListener('load', function() {
     console.log('On Load.');
 
-    var audioCtx = new AudioContext();
+    var audioCtx = window.__audioCtx = new AudioContext();
+
+    navigator.getUserMedia({ audio: { optional: [ { echoCancellation: false } ] } },
+      function(stream) {
+        console.log('stream.');
+        var source = window.__source = audioCtx.createMediaStreamSource(stream);
+
+        var volume = audioCtx.createGain();
+        volume.gain.value = 2.0;
+
+        source.connect(volume);
+        volume.connect(audioCtx.destination);
+      },
+      function() {
+        // error
+        console.log(arguments);
+      });
+
   });
 })();
