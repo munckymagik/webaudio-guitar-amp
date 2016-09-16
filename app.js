f583be52971fe5fe9f7189fcc6a478a9f439558a
Dan Munckton <dan@cultivatehq.com>
2016-09-16 21:43:38 +0100

    DynamicsCompressorNode.reduction is now readonly

--- app.js
+++ app.js
@@ -187,7 +187,6 @@
     compressor.threshold.value = -50;
     compressor.knee.value = 40;
     compressor.ratio.value = 12;
-    compressor.reduction.value = -20;
     compressor.attack.value = 0.25;
     compressor.release.value = 0.25;
 
