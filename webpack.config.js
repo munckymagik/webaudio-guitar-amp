4e8a81308635367b0be57617ef54ef228f3e54d9
Dan Munckton <dan@cultivatehq.com>
2016-09-17 16:21:13 +0100

    Integrate babel with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,10 @@ module.exports = {
   output: {
     path: __dirname,
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
   }
 }
