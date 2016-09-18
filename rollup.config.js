c147976a9336657dc203f4266541b42718e2327e
Dan Munckton <dan@cultivatehq.com>
2016-09-18 11:00:26 +0100

    Configure rollup to run babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
+import babel from 'rollup-plugin-babel'
+
 export default {
   entry: 'src/main.js',
   dest: 'bundle.js',
   sourceMap: true,
-  format: 'iife'
+  format: 'iife',
+  plugins: [babel()]
 };
