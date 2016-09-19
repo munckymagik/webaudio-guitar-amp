1eedd77fce65ca637d4b236f6d8d7203b035457e
Dan Munckton <dan@cultivatehq.com>
2016-09-20 00:21:08 +0100

    ESLint manual fixes

--- src/modules/Distortion.js
+++ src/modules/Distortion.js
@@ -1,14 +1,14 @@
 // Taken from: https://developer.mozilla.org/en-US/docs/Web/API/WaveShaperNode
 function makeDistortionCurve(amount) {
   const k = typeof amount === 'number' ? amount : 50
-  const n_samples = 44100
-  const curve = new Float32Array(n_samples)
+  const nSamples = 44100
+  const curve = new Float32Array(nSamples)
   const deg = Math.PI / 180
   let i = 0
   let x
 
-  for (; i < n_samples; ++i) {
-    x = i * 2 / n_samples - 1
+  for (; i < nSamples; ++i) {
+    x = i * 2 / nSamples - 1
     curve[i] = (3 + k) * x * 20 * deg / (Math.PI + k * Math.abs(x))
   }
 
@@ -23,20 +23,20 @@ class Distortion {
     this.toneCtrl = uiElement.querySelector('.js-distortion-tone')
 
     this.distortion = audioCtx.createWaveShaper()
-    this.distortion.curve = makeDistortionCurve(parseInt(this.amountCtrl.value))
+    this.distortion.curve = makeDistortionCurve(parseInt(this.amountCtrl.value, 10))
     this.distortion.oversample = '4x'
 
     this.lowPass = audioCtx.createBiquadFilter()
     this.lowPass.type = 'lowpass'
-    this.lowPass.frequency.value = parseInt(this.toneCtrl.value)
+    this.lowPass.frequency.value = parseInt(this.toneCtrl.value, 10)
 
     this.distortion.connect(this.lowPass)
 
     this.amountCtrl.addEventListener('change', () => {
-      self.distortion.curve = makeDistortionCurve(parseInt(self.amountCtrl.value))
+      self.distortion.curve = makeDistortionCurve(parseInt(self.amountCtrl.value, 10))
     })
     this.toneCtrl.addEventListener('input', () => {
-      self.lowPass.frequency.value = parseInt(self.toneCtrl.value)
+      self.lowPass.frequency.value = parseInt(self.toneCtrl.value, 10)
     })
   }
 
