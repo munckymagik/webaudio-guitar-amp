3fccb5961fea0c08864b741441c8b0586d5268a1
Dan Munckton <dan@cultivatehq.com>
2017-02-28 20:39:34 +0000

    Fixes for tslint upgrade

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,16 @@ import loadUserMediaSource from './sources/loadUserMediaSource'
 function app() {
   console.log('Starting application.')
 
-  let audioCtx = new AudioContext()
-  let signalChain = buildSignalChain(audioCtx)
+  const audioCtx = new AudioContext()
+  const signalChain = buildSignalChain(audioCtx)
 
-  let soundFilePromise = loadSoundFileSource(audioCtx, signalChain)
-  let guitarInputPromise = loadUserMediaSource(audioCtx, signalChain)
+  const soundFilePromise = loadSoundFileSource(audioCtx, signalChain)
+  const guitarInputPromise = loadUserMediaSource(audioCtx, signalChain)
 
   Promise.all([soundFilePromise, guitarInputPromise]).then((...args) => {
     console.log('All sources loaded')
     console.log(args)
-  }).catch(error => {
+  }).catch((error) => {
     console.log('Error source loading failed', error)
   })
 
